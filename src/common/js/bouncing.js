0a6adba81910341b181cb0f03296439b9b7d1667
LiuLongTian <1750625203@qq.com>
2018-10-08 13:41:17 +0800

    no message

---
+++ src/common/js/bouncing.js
@@ -0,0 +1,81 @@
+
+import Vue from 'vue'
+
+// 注册一个全局自定义指令 `v-bouncing` 用于加载时
+/**
+        * .el-loading-mask {
+        * position: absolute;
+        * z-index: 2000;
+        * background-color: rgba(255,255,255,.9);
+        * margin: 0;
+        * top: 0;
+        * right: 0;
+        * bottom: 0;
+        * left: 0;
+        * -webkit-transition: opacity .3s;
+        *  transition: opacity .3s;
+        * }
+        */
+
+        // v-bouncing 不可用在子组件template下的首级div
+        Vue.directive('bouncing', {
+            bind: function (el, binding) {
+              let bouncing_loader = document.createElement("div");
+              let div1 = document.createElement("div");
+              let div2 = document.createElement("div");
+              let div3 = document.createElement("div");
+              bouncing_loader.className = 'flex  flex-align-center flex-pack-center el-loading-mask bouncing-loader ';
+              bouncing_loader.appendChild(div1);
+              bouncing_loader.appendChild(div2);
+              bouncing_loader.appendChild(div3);
+              el.appendChild(bouncing_loader)
+          
+            },
+            // 当被绑定的元素插入到 DOM 中时……
+            inserted: function (el, binding) {
+            },
+            // 所在组件的 VNode 更新时调用，但是可能发生在其子 VNode 更新之前。指令的值可能发生了改变，也可能没有。但是你可以通过比较更新前后的值来忽略不必要的模板更新 (详细的钩子函数参数见下)。
+            update: function (el, binding) {
+            },
+            // 指令所在组件的 VNode 及其子 VNode 全部更新后调用。
+            componentUpdated: function (el, binding) {
+          
+              if (!el.getElementsByClassName('bouncing-loader')[0]) {
+                let bouncing_loader = document.createElement("div");
+                let div1 = document.createElement("div");
+                let div2 = document.createElement("div");
+                let div3 = document.createElement("div");
+                bouncing_loader.className = 'flex  flex-align-center flex-pack-center el-loading-mask bouncing-loader ';
+                bouncing_loader.appendChild(div1);
+                bouncing_loader.appendChild(div2);
+                bouncing_loader.appendChild(div3);
+                el.appendChild(bouncing_loader)
+              }
+          
+              let bouncing_loader = el.getElementsByClassName('bouncing-loader')[0]
+           let loader =  ()=>{
+              if (binding.value) {
+               
+                if( el.className.indexOf('bouncing-position')===-1){
+                  el.className += ' bouncing-position'
+                }
+                if(bouncing_loader.className.indexOf('bouncing-none') !== -1){
+                  bouncing_loader.className = bouncing_loader.className.replace('bouncing-none', '').trim()
+                }
+              } else{
+                bouncing_loader.className += ' bouncing-none'
+                if(bouncing_loader.className.indexOf('bouncing-none') !== -1){
+                  el.className = el.className.replace('bouncing-position', '').trim()
+                }
+              
+              }
+             }
+              loader();
+            },
+            unbind: function (el, binding) {
+          
+            }
+          })
+
+
+          
--- src/main.ts
+++ src/main.ts
@@ -13,6 +13,7 @@ import 'quill/dist/quill.bubble.css'
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
 import 'typeface-roboto'
+import './common/js/bouncing'
 
 Vue.use(ElementUI);
 Vue.config.productionTip = false
@@ -53,81 +54,7 @@ Vue.prototype.dohavePermission = PermissionItem => {
 };
 
 
-// 注册一个全局自定义指令 `v-bouncing` 用于加载时
-/**
-        * .el-loading-mask {
-        * position: absolute;
-        * z-index: 2000;
-        * background-color: rgba(255,255,255,.9);
-        * margin: 0;
-        * top: 0;
-        * right: 0;
-        * bottom: 0;
-        * left: 0;
-        * -webkit-transition: opacity .3s;
-        *  transition: opacity .3s;
-        * }
-        */
 
-        // v-bouncing 不可用在子组件template下的首级div
-Vue.directive('bouncing', {
-  bind: function (el, binding) {
-    let bouncing_loader = document.createElement("div");
-    let div1 = document.createElement("div");
-    let div2 = document.createElement("div");
-    let div3 = document.createElement("div");
-    bouncing_loader.className = 'flex  flex-align-center flex-pack-center el-loading-mask bouncing-loader ';
-    bouncing_loader.appendChild(div1);
-    bouncing_loader.appendChild(div2);
-    bouncing_loader.appendChild(div3);
-    el.appendChild(bouncing_loader)
-
-  },
-  // 当被绑定的元素插入到 DOM 中时……
-  inserted: function (el, binding) {
-    // el.style.opacity = binding.value?'0.2':'1'
-  },
-  update: function (el, binding) {
-  },
-  componentUpdated: function (el, binding) {
-
-    if (!el.getElementsByClassName('bouncing-loader')[0]) {
-      let bouncing_loader = document.createElement("div");
-      let div1 = document.createElement("div");
-      let div2 = document.createElement("div");
-      let div3 = document.createElement("div");
-      bouncing_loader.className = 'flex  flex-align-center flex-pack-center el-loading-mask bouncing-loader ';
-      bouncing_loader.appendChild(div1);
-      bouncing_loader.appendChild(div2);
-      bouncing_loader.appendChild(div3);
-      el.appendChild(bouncing_loader)
-    }
-
-    let bouncing_loader = el.getElementsByClassName('bouncing-loader')[0]
-
- let loader =  ()=>{
-    if (binding.value) {
-     
-      if( el.className.indexOf('bouncing-position')===-1){
-        el.className += ' bouncing-position'
-      }
-      if(bouncing_loader.className.indexOf('bouncing-none') !== -1){
-        bouncing_loader.className = bouncing_loader.className.replace('bouncing-none', '').trim()
-      }
-    } else{
-      bouncing_loader.className += ' bouncing-none'
-      if(bouncing_loader.className.indexOf('bouncing-none') !== -1){
-        el.className = el.className.replace('bouncing-position', '').trim()
-      }
-    
-    }
-   }
-    loader();
-  },
-  unbind: function (el, binding) {
-
-  }
-})
 
 /* eslint-disable no-new */
 new Vue({
